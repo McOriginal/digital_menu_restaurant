8298a08db06ac32baac4b644f5fbeeff0723493b
McOriginal <originalmc223@gmail.com>
2025-12-22 20:31:11 +0000

    Correction de design

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -4,16 +4,20 @@ import Meals from '../Components/Meals';
 export default function AdminPage() {
   return (
     <>
-      <header className='bg-white shadow-md p-4 flex justify-between items-center'>
+      {/* <header className='bg-white shadow-md p-4 flex justify-between items-center'>
         <h1 className='text-2xl font-bold text-red-500'>Cisse Délice</h1>
-        <Link
-          to='/orders'
-          className='bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition'
-        >
-          Voir les commandes
-        </Link>
-      </header>
-      <Meals />
+       
+      </header> */}
+      <Meals
+        btn={
+          <Link
+            to='/orders'
+            className='bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition'
+          >
+            Voir les commandes
+          </Link>
+        }
+      />
     </>
   );
 }
